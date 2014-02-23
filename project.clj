2d64e422d77a048e4bbd208060cf1720bc8fd9d1
Ryan Thomas <rthomas@atlassian.com>
2014-02-23 14:21:28 +1100

    Added scm details

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject io.floop/figgus "0.1.0-SNAPSHOT"
   :description "A library for reading configuration from system properties and other locations."
   :url "http://github.com/floopio/figgus"
+  :scm {:name "git"
+        :url "git@github.com:floopio/figgus.git"}
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
