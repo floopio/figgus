fd4302c6dc28a0d41335d04a1022606a1543c1c9
Ryan Thomas <rthomas@atlassian.com>
2014-02-23 14:36:00 +1100

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.floop/figgus "0.1.0"
+(defproject io.floop/figgus "0.1.1-SNAPSHOT"
   :description "A library for reading configuration from system properties and other locations."
   :url "http://github.com/floopio/figgus"
   :scm {:name "git"
@@ -9,4 +9,5 @@
                  [cheshire "5.2.0"]
                  [log4j/log4j "1.2.17"]
                  [org.clojure/tools.logging "0.2.6"]]
-  :lein-release {:deploy-via :clojars})
+  :lein-release {:deploy-via :shell
+                 :shell ["lein" "deploy" "clojars"]})
