ff06a359685528a43bd1f95b6b58fc2e3ad5268f
Ryan Thomas <rthomas@atlassian.com>
2014-03-17 17:06:05 +1100

    lein-release plugin: bumped version from 0.2.3 to 0.2.4-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.floop/figgus "0.2.3"
+(defproject io.floop/figgus "0.2.4-SNAPSHOT"
   :description "A library for reading configuration from system properties and other locations."
   :url "http://github.com/floopio/figgus"
   :scm {:name "git"
