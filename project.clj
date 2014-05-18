9ba4af2998835c5536fb766d0ec69453270021ae
Ryan Thomas <rthomas@atlassian.com>
2014-05-19 06:39:30 +1000

    lein-release plugin: preparing 0.2.6 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.floop/figgus "0.2.6-SNAPSHOT"
+(defproject io.floop/figgus "0.2.6"
   :description "A library for reading configuration from system properties and other locations."
   :url "http://github.com/floopio/figgus"
   :scm {:name "git"
